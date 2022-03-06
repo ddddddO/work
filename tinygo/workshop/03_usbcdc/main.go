68add27bb62b0491cb93c28e0e39c847a1c0287b
ddddddO <lbfdeatq@gmail.com>
2022-03-06 17:36:49 +0900

    add usb cdc

---
+++ tinygo/workshop/03_usbcdc/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"machine"
+	"time"
+)
+
+func main() {
+	led := machine.LCD_BACKLIGHT
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	led.High()
+
+	usbcdc := machine.Serial
+	usbcdc.Configure(machine.UARTConfig{})
+
+	input := make([]byte, 64)
+	i := 0
+	for {
+		if usbcdc.Buffered() > 0 {
+			data, _ := usbcdc.ReadByte()
+
+			switch data {
+			case 13:
+				// return key
+				usbcdc.Write([]byte("\r\n"))
+
+				switch string(input[:i]) {
+				case "on":
+					led.High()
+					fmt.Print("aaaaa\r\n")
+					usbcdc.Write([]byte("xxx"))
+				case "off":
+					led.Low()
+				case "toggle", "t":
+					led.Toggle()
+				}
+				i = 0
+			default:
+				// just echo the character
+				usbcdc.WriteByte(data)
+				input[i] = data
+				i++
+			}
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+}
