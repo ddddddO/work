ccb68a0123aff943f3c354b05ec978b67797278c
ddddddO <lbfdeatq@gmail.com>
2022-03-06 13:28:29 +0900

    add buzzzer

--- tinygo/workshop/01_blinky/main.go
+++ tinygo/workshop/01_blinky/main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"machine"
 	"time"
+
+	"tinygo.org/x/drivers/buzzer"
 )
+
+type note struct {
+	tone     float64
+	duration float64
+}
 
 func main() {
 	// led := machine.LED
 	led := machine.LCD_BACKLIGHT
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	cnt := 0
+
+	bzrPin := machine.WIO_BUZZER
+	bzrPin.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	bzr := buzzer.New(bzrPin)
+	notes := []note{
+		{buzzer.C3, buzzer.Quarter},
+		{buzzer.D3, buzzer.Quarter},
+		{buzzer.E3, buzzer.Quarter},
+		{buzzer.A3, buzzer.Quarter},
+		{buzzer.G7, buzzer.Quarter},
+	}
+
 	for {
+		cnt++
+
+		// ref: https://github.com/sago35/tinygo-workshop#println-%E3%81%A8-fmtprint
+		// 「TinyGo では println や fmt.Print 等で出力される先は USB-CDC or UART となっていて、 Wio Terminal は USB-CDC を使うように初期設定されています」
+		// yterm --target wioterminal でPC側で出力を確認できる
+		fmt.Printf("cnt: %d\r\n", cnt)
+
 		led.Low()
 		time.Sleep(time.Millisecond * 1000)
 
 		led.High()
-		time.Sleep(time.Millisecond * 500)
+		time.Sleep(time.Millisecond * 1500)
+
+		for _, n := range notes {
+			bzr.Tone(n.tone, n.duration)
+			time.Sleep(10 * time.Millisecond)
+		}
 	}
 }
--- tinygo/workshop/README.md
+++ tinygo/workshop/README.md
@@ -1,8 +1,16 @@
 refs:
 - https://github.com/sago35/tinygo-workshop
 - [TinyGo, VSCode, gopls, wioterminal](https://qiita.com/sago35/items/c30cbce4a0a3e12d899c)
+- 回路図の確認
+    >tips: 回路図を確認する
+    組込みにおいて各種センサー等がどこにどのように繋がっているかを確認しておく必要があります。 Wio Terminal の場合は以下にまとまっているので確認しておくと良いです。上記の図は、下記 pdf からの抜粋になります。
+
+    - https://wiki.seeedstudio.com/Wio-Terminal-Getting-Started/
+    - https://files.seeedstudio.com/wiki/Wio-Terminal/res/Wio-Terminal-SCH-v1.2.pdf
+
 
 # メモ
+- VSCodeから開けるPowershellだとうまくいかん
 - Powershellから`code .` でvs codeを開く
 - Powershellから書き込む
     - e.g. ` tinygo flash --target wioterminal --size short .\01_blinky\`
