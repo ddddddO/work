00e5181a713228a74905049c7e1f426d0bb6f946
ddddddO <lbfdeatq@gmail.com>
2022-03-06 17:06:49 +0900

    refactor gpio

--- tinygo/workshop/02_gpio/main.go
+++ tinygo/workshop/02_gpio/main.go
@@ -10,46 +10,15 @@ const (
 	led = machine.LCD_BACKLIGHT
 )
 
-func main() {
-	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+// machine.でグローバルにアクセスできてしまうけど一応構造体で持っておく
 
-	buttonA, buttonB, buttonC := prepareButton()
-	up, down, left, right, press := prepareJuziKey()
-
-	for {
-		if !buttonA.Get() {
-			led.Low()
-			fmt.Printf("machine.WIO_KEY_A pressed\r\n")
-		} else if !buttonB.Get() {
-			led.Low()
-			fmt.Printf("machine.WIO_KEY_B pressed\r\n")
-		} else if !buttonC.Get() {
-			led.Low()
-			fmt.Printf("machine.WIO_KEY_C pressed\r\n")
-		} else if !up.Get() {
-			led.Low()
-			fmt.Printf("machine.WIO_5S_UP pressed\r\n")
-		} else if !left.Get() {
-			led.Low()
-			fmt.Printf("machine.WIO_5S_LEFT pressed\r\n")
-		} else if !right.Get() {
-			led.Low()
-			fmt.Printf("machine.WIO_5S_RIGHT pressed\r\n")
-		} else if !down.Get() {
-			led.Low()
-			fmt.Printf("machine.WIO_5S_DOWN pressed\r\n")
-		} else if !press.Get() {
-			led.Low()
-			fmt.Printf("machine.WIO_5S_PRESS pressed\r\n")
-		} else {
-			led.High()
-		}
-
-		time.Sleep(time.Millisecond * 10)
-	}
+type button struct {
+	a machine.Pin
+	b machine.Pin
+	c machine.Pin
 }
 
-func prepareButton() (machine.Pin, machine.Pin, machine.Pin) {
+func newButton() button {
 	buttonA := machine.WIO_KEY_A
 	buttonA.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
 
@@ -58,10 +27,35 @@ func prepareButton() (machine.Pin, machine.Pin, machine.Pin) {
 
 	buttonC := machine.WIO_KEY_C
 	buttonC.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
-	return buttonA, buttonB, buttonC
+
+	return button{
+		a: buttonA,
+		b: buttonB,
+		c: buttonC,
+	}
 }
 
-func prepareJuziKey() (machine.Pin, machine.Pin, machine.Pin, machine.Pin, machine.Pin) {
+func (b button) isPressedA() bool {
+	return !b.a.Get()
+}
+
+func (b button) isPressedB() bool {
+	return !b.b.Get()
+}
+
+func (b button) isPressedC() bool {
+	return !b.c.Get()
+}
+
+type juziKey struct {
+	up    machine.Pin
+	down  machine.Pin
+	left  machine.Pin
+	right machine.Pin
+	press machine.Pin
+}
+
+func newJuziKey() juziKey {
 	up := machine.WIO_5S_UP
 	up.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
 
@@ -77,5 +71,70 @@ func prepareJuziKey() (machine.Pin, machine.Pin, machine.Pin, machine.Pin, machi
 	press := machine.WIO_5S_PRESS
 	press.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
 
-	return up, down, left, right, press
+	return juziKey{
+		up:    up,
+		down:  down,
+		left:  left,
+		right: right,
+		press: press,
+	}
+}
+
+func (j juziKey) isPressedUp() bool {
+	return !j.up.Get()
+}
+
+func (j juziKey) isPressedDown() bool {
+	return !j.down.Get()
+}
+
+func (j juziKey) isPressedLeft() bool {
+	return !j.left.Get()
+}
+
+func (j juziKey) isPressedRight() bool {
+	return !j.right.Get()
+}
+
+func (j juziKey) isPressedPress() bool {
+	return !j.press.Get()
+}
+
+func main() {
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	button := newButton()
+	juziKey := newJuziKey()
+
+	for {
+		if button.isPressedA() {
+			led.Low()
+			fmt.Printf("machine.WIO_KEY_A pressed\r\n")
+		} else if button.isPressedB() {
+			led.Low()
+			fmt.Printf("machine.WIO_KEY_B pressed\r\n")
+		} else if button.isPressedC() {
+			led.Low()
+			fmt.Printf("machine.WIO_KEY_C pressed\r\n")
+		} else if juziKey.isPressedUp() {
+			led.Low()
+			fmt.Printf("machine.WIO_5S_UP pressed\r\n")
+		} else if juziKey.isPressedLeft() {
+			led.Low()
+			fmt.Printf("machine.WIO_5S_LEFT pressed\r\n")
+		} else if juziKey.isPressedRight() {
+			led.Low()
+			fmt.Printf("machine.WIO_5S_RIGHT pressed\r\n")
+		} else if juziKey.isPressedDown() {
+			led.Low()
+			fmt.Printf("machine.WIO_5S_DOWN pressed\r\n")
+		} else if juziKey.isPressedPress() {
+			led.Low()
+			fmt.Printf("machine.WIO_5S_PRESS pressed\r\n")
+		} else {
+			led.High()
+		}
+
+		time.Sleep(time.Millisecond * 10)
+	}
 }
