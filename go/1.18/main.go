48ba33f8ef6b557fa03c38289ee9f2eb1dc104a2
ddddddO <lbfdeatq@gmail.com>
2022-03-19 12:42:38 +0900

    add

--- go/1.18/main.go
+++ go/1.18/main.go
@@ -18,13 +18,34 @@ func main() {
 		"second": 26.99,
 	}
 
-	// fmt.Printf("Non-Generic Sums: %v and %v\n",
-	// 	SumInts(ints),
-	// 	SumFloats(floats))
+	fmt.Printf("Non-Generic Sums: %v and %v\n",
+		SumInts(ints),
+		SumFloats(floats))
+
 	fmt.Printf("Generic Sums: %v and %v\n",
 		SumIntsOrFloats[string, int64](ints),
 		SumIntsOrFloats[string, float64](floats),
 	)
+
+	fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
+
+	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
+		SumNumbers(ints),
+		SumNumbers(floats))
+}
+
+type Number interface {
+	int64 | float64
+}
+
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
 }
 
 func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
