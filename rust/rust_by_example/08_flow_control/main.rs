b27d67fc0d9e48b69cdd9d3b3f89e74d9f4026da
ddddddO <lbfdeatq@gmail.com>
2023-02-26 00:30:57 +0900

    Next 関数: https://doc.rust-jp.rs/rust-by-example-ja/fn.html

--- rust/rust_by_example/08_flow_control/main.rs
+++ rust/rust_by_example/08_flow_control/main.rs
@@ -1,9 +1,13 @@
 #![allow(unreachable_code)]
 
+// TODO: 以下以降の8章もう一度目を通していいかも
+// https://doc.rust-jp.rs/rust-by-example-ja/flow_control/match.html
 fn main() {
   ifelse_fn();
   loop_fn();
   label_fn();
+  ret_loop_fn();
+  for_fn();
 }
 
 fn ifelse_fn() {
@@ -63,4 +67,35 @@ fn label_fn() {
   // outer loop
   // inner loop
   // end
+}
+
+fn ret_loop_fn() {
+  let mut count = 0;
+  let ret = loop {
+    count += 1;
+
+    if count == 10 {
+      break count * 2;
+    }
+  };
+  println!("count: {}", ret);
+  // count: 20
+}
+
+fn for_fn() {
+  for n in 0..3 {
+    println!("a: {}", n);
+  }
+
+  for n in 0..=3 {
+    println!("b: {}", n);
+  }
+
+  // a: 0
+  // a: 1
+  // a: 2
+  // b: 0
+  // b: 1
+  // b: 2
+  // b: 3
 }
