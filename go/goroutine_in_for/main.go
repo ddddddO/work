fe1930e241db259dea0aee139b8266b20257d0aa
ddddddO <lbfdeatq@gmail.com>
2023-02-25 09:55:14 +0900

    modify

--- go/goroutine_in_for/main.go
+++ go/goroutine_in_for/main.go
@@ -1,3 +1,6 @@
+// NOTE: こちらの挙動について確認
+// https://github.com/hnakamur/httpcapt/issues/1
+// どうやら、interface越しに呼ばれるとfor文内で変数に束縛せずにgoroutineで処理できるよう
 package main
 
 import (
@@ -6,8 +9,16 @@ import (
 	"time"
 )
 
+type ifHandler interface {
+	handle(ctx context.Context, result <-chan string)
+}
+
+func run(ifh ifHandler, ctx context.Context, result <-chan string) {
+	ifh.handle(ctx, result)
+}
+
 type multiHandler struct {
-	handlers []handler
+	handlers []*handler
 }
 
 func (mh *multiHandler) handle(ctx context.Context, result <-chan string) {
@@ -22,29 +33,29 @@ type handler struct {
 
 func (h *handler) handle(_ context.Context, result <-chan string) {
 	<-result
+	time.Sleep(time.Duration(h.n) * time.Second)
 	fmt.Println(h.n)
 }
 
 func main() {
-	a := handler{n: 10}
-	b := handler{n: 20}
-	c := handler{n: 40}
-	hs := []handler{a, b, c}
-	mh := multiHandler{handlers: hs}
+	a := &handler{n: 1}
+	b := &handler{n: 2}
+	c := &handler{n: 3}
+	hs := []*handler{a, b, c}
+	mh := &multiHandler{handlers: hs}
 
 	ctx := context.Background()
 	result := make(chan string)
 
-	mh.handle(ctx, result)
-	time.Sleep(1 * time.Second)
+	run(mh, ctx, result)
 	result <- "aaa"
 	result <- "aaa"
 	result <- "aaa"
 
-	time.Sleep(3 * time.Second)
+	time.Sleep(4 * time.Second)
 	fmt.Println("end!")
-	// 40
-	// 40
-	// 40
+	// 1
+	// 2
+	// 3
 	// end!
 }
