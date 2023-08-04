2ec942a19b064eac87a9fc611173a9b7c104e2e4
ddddddO <lbfdeatq@gmail.com>
2023-08-05 08:20:15 +0900

    filesystem to markdowm

--- go/cmd/fs/main.go
+++ go/cmd/fs/main.go
@@ -5,20 +5,11 @@ import (
 	"io/fs"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	fileSystem := os.DirFS("../../../../gtree/example")
-	err := fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(path)
-		return nil
-	})
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
+	normal()
 	// Output:
 	// .
 	// find_pipe_programmable-gtree
@@ -32,4 +23,61 @@ func main() {
 	// like_cli/main.go
 	// programmable
 	// programmable/main.go
+
+	fmt.Println()
+
+	filesystemToMarkdown()
+	// Output:
+	//- .
+	// 	- find_pipe_programmable-gtree
+	// 					- main.go
+	// 	- go-list_pipe_programmable-gtree
+	// 					- main.go
+	// 	- like_cli
+	// 					- adapter
+	// 									- executor.go
+	// 									- indentation.go
+	// 					- main.go
+	// 	- noexist
+	// 					- xxx
+	// 	- programmable
+	// 					- main.go
+}
+
+func normal() {
+	fileSystem := os.DirFS("../../../../gtree/example")
+	err := fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(path)
+		return nil
+	})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
+
+func filesystemToMarkdown() {
+	const indent = "	"
+	fileSystem := os.DirFS("../../../../gtree/example")
+	startPoint := "."
+	err := fs.WalkDir(fileSystem, startPoint, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		names := strings.Split(path, "/")
+
+		line := fmt.Sprintf("%s- %s", strings.Repeat(indent, strings.Count(path, "/")+1), names[len(names)-1])
+		if names[len(names)-1] == startPoint {
+			line = fmt.Sprintf("%s- %s", strings.Repeat(indent, strings.Count(path, "/")), names[len(names)-1])
+		}
+
+		fmt.Println(line)
+		return nil
+	})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
 }
